a79c6fdbea0c8b3c990f468f5a3eef5374872202
Irina <i.tyapysheva@gmail.com>
2019-04-12 22:53:27 +0400

    Update database
    Delete join-table
    Add comment

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,65 +1,69 @@
 create table if not exists organization(
-    id          integer AUTO_INCREMENT PRIMARY KEY,
+    id          integer AUTO_INCREMENT PRIMARY KEY COMMENT 'Уникальный идентификатор',
     version     integer not null COMMENT 'Служебное поле hibernate',
-    name        varchar(50),
-    full_name    varchar(200),
-    inn         varchar(12),
-    kpp         varchar(9),
-    address     varchar(200),
-    phone       varchar(15),
-    active      boolean
+    name       varchar(50) COMMENT 'Название организации',
+    full_name    varchar(200) COMMENT 'Полное название организации',
+    inn         varchar(12) COMMENT 'ИНН организации',
+    kpp         varchar(9)COMMENT 'КПП организации',
+    address     varchar(200)COMMENT 'Адрес организации',
+    phone       varchar(15) COMMENT 'Телефон организации',
+    active      boolean COMMENT 'Действующая организация'
 );
+COMMENT ON TABLE organization IS 'Организация'
+----------------------------------------
 create table if not exists office(
-  id integer AUTO_INCREMENT PRIMARY KEY,
+  id integer AUTO_INCREMENT PRIMARY KEY COMMENT 'Уникальный идентификатор',
   version  integer not null COMMENT 'Служебное поле hibernate',
-  name varchar (50),
-  address varchar (200),
-  phone varchar (15),
-  version integer,
-  active boolean,
-  org_id integer,
+  name varchar (50) COMMENT 'Название офиса',
+  address varchar (200) COMMENT 'Адрес офиса',
+  phone varchar (15)COMMENT 'Телефон офиса',
+  active boolean COMMENT 'Действующий офис',
+  org_id integer COMMENT 'Уникальный идентификатор организации',
   foreign key(org_id) references organization(id)
 );
+COMMENT ON TABLE office IS 'Офис'
+----------------------------------------------
 create table if not exists citizenship(
-  id integer AUTO_INCREMENT PRIMARY KEY,
+  id integer AUTO_INCREMENT PRIMARY KEY COMMENT 'Уникальный идентификатор',
   version  integer not null COMMENT 'Служебное поле hibernate',
-  code smallint,
-  name varchar (50)
+  code varchar(4) COMMENT 'Код страны',
+  name varchar (50) COMMENT 'Название страны'
 );
-create table if not exists document(
-  id integer AUTO_INCREMENT PRIMARY KEY,
+COMMENT ON TABLE citizenship IS 'Гражданство'
+----------------------------------------------
+create table if not exists document_type(
+  id integer AUTO_INCREMENT PRIMARY KEY COMMENT 'Уникальный идентификатор',
   version  integer not null COMMENT 'Служебное поле hibernate',
-  code tinyint,
-  name varchar (150)
+  code varchar(4) COMMENT 'Код типа документа',
+  name varchar (150) COMMENT 'Название типа документа'
 );
-
+COMMENT ON TABLE document_type IS 'Тип документа'
+-------------------------------------------------
 create table if not exists person(
-  id integer AUTO_INCREMENT PRIMARY KEY,
+  id integer AUTO_INCREMENT PRIMARY KEY COMMENT 'Уникальный идентификатор',
   version  integer not null COMMENT 'Служебное поле hibernate',
-  first_name varchar (25),
-  middle_name varchar (25),
-  second_name varchar (25),
-  phone varchar (15),
-  position varchar (50),
-  identified boolean,
-  office_id integer,
-  foreign key(office_id)references office(id)
+  first_name varchar (25)COMMENT 'Имя',
+  middle_name varchar (25)COMMENT 'Отчество',
+  second_name varchar (25)COMMENT 'Фамилия',
+  phone varchar (15)COMMENT 'Телефон',
+  position varchar (50)COMMENT 'Должность',
+  identified boolean COMMENT 'Потвержденный документ',
+  id_office integer COMMENT 'Уникальный идентификатор офиса',
+  id_citizenship integer COMMENT 'Уникальный идентификатор гражданства',
+  id_doc integer COMMENT 'Уникальный идентификатор документа',
+  foreign key(id_office)references office(id),
+  foreign key(id_doc)references document(id),
+  foreign key(id_citizenship)references citizenship(id)
 );
+COMMENT ON TABLE person IS 'Человек'
+--------------------------------------------------
+create table if not exists document(
+id integer COMMENT 'Уникальный идентификатор',
+doc_number varchar(50)COMMENT 'Номер документа',
+doc_date date COMMENT 'Дата получения документа',
+id_doctype integer COMMENT 'Уникальный идентификатор типа документа',
+foreign key(id_doctype)references document_type(id),
+);
+COMMENT ON TABLE document IS 'Документ';
 
-create table if not exists person_doc(
-id integer AUTO_INCREMENT PRIMARY KEY,
-doc_number varchar(50),
-doc_date date,
-id_person integer,
-id_doc integer,
-foreign key(id_doc)references document(id),
-foreign key(id_person)references person(id)
-);
 
-create table if not exists person_citizenship(
-id integer AUTO_INCREMENT PRIMARY KEY,
-id_person integer,
-id_citizenship integer,
-foreign key(id_citizenship)references citizenship(id),
-foreign key(id_person)references person(id)
-);
